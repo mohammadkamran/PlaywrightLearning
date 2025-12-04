12a2c55838123fe677bc7b18f801fba2a3901603
kamran <md.kamran2k6@gmail.com>
2025-12-05 04:59:32 +0530

    create post test case added

---
+++ tests/api/create-api.test.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("API create Request Demo", () => {
+  test("TC_001: Validate create request", async ({ request }) => {
+    const requestBody = {
+      firstname: "Jim",
+      lastname: "Brown",
+      totalprice: 111,
+      depositpaid: true,
+      bookingdates: {
+        checkin: "2018-01-01",
+        checkout: "2019-01-01",
+      },
+      additionalneeds: "Breakfast",
+    };
+    const response = await request.post(
+      "https://restful-booker.herokuapp.com/booking",
+      { data: requestBody }
+    );
+    const responseBody = await response.json();
+    console.log("response body: ", responseBody);
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    expect(responseBody).toHaveProperty("bookingid");
+    expect(responseBody).toHaveProperty("booking");
+    expect(responseBody).toHaveProperty("booking.additionalneeds");
+    const bookingid = responseBody.bookingid;
+    const booking = responseBody.booking;
+    console.log("bookingid: ", bookingid)
+    console.log("booking: ", booking);
+    const bookingdates = responseBody.booking.bookingdates;
+    console.log("booking dates: ", bookingdates)
+    const additionalneeds = responseBody.booking.additionalneeds;
+    console.log("additional needs: ", additionalneeds)
+  });
+});
