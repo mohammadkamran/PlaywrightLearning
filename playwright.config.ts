2e5420f828370ee9287d15528fcda3fe7149042d
kamran <md.kamran2k6@gmail.com>
2025-12-06 19:14:18 +0530

    create api json request added

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html',{open:'on-failure'}],['dot'],['line'],['list']],
+  reporter: [['html',{open:'always'}]],
+  //reporter: [['dot'],['line'],['list']],
   //reporter: ['junit',{outputFile:'result.xml'}]
   //reporter: ['json',{outputFile:'result.json'}]
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/api/create-api.test.ts
+++ tests/api/create-api.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import requestBody from '../api/api-json-testData/request.json';
 
 test.describe("API create Request Demo", () => {
   test("TC_001: Validate create request", async ({ request }) => {
@@ -22,6 +23,25 @@ test.describe("API create Request Demo", () => {
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
     expect(responseBody).toHaveProperty("bookingid");
+    expect(responseBody).toHaveProperty("booking");
+    expect(responseBody).toHaveProperty("booking.additionalneeds");
+    const bookingid = responseBody.bookingid;
+    const booking = responseBody.booking;
+    console.log("bookingid: ", bookingid)
+    console.log("booking: ", booking);
+    const bookingdates = responseBody.booking.bookingdates;
+    console.log("booking dates: ", bookingdates)
+    const additionalneeds = responseBody.booking.additionalneeds;
+    console.log("additional needs: ", additionalneeds)
+  });
+
+  test('TC_002: Validate create request through JOSN body',async({request})=>{
+    const response = await request.post("https://restful-booker.herokuapp.com/booking",{data:requestBody});
+     const responseBody = await response.json();
+    console.log("response body: ", responseBody);
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+     expect(responseBody).toHaveProperty("bookingid");
     expect(responseBody).toHaveProperty("booking");
     expect(responseBody).toHaveProperty("booking.additionalneeds");
     const bookingid = responseBody.bookingid;
