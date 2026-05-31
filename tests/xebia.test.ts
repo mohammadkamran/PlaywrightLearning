65292a09d287a63972a868acf31efed9d2f1fc3e
kamran <md.kamran2k6@gmail.com>
2026-06-01 04:18:59 +0530

    xebia.test.ts code updated

--- tests/xebia.test.ts
+++ tests/xebia.test.ts
@@ -1,20 +1,30 @@
-import {test,expect} from '@playwright/test';
-test("Handle multiple windows",async({page,context})=>{
-    await page.goto("https://the-internet.herokuapp.com/windows");
-    //console.log('Parent Page Title:', await page.title());
-    const [newPage] = await Promise.all([
-        context.waitForEvent('page'),
-         await page.locator('a[href="/windows/new"]').click()
-    ]);
-    await newPage.waitForLoadState();
+import { test, expect } from "@playwright/test";
 
-    const text = await newPage.locator('h3').textContent();
-    console.log("New Page Title:" ,text);
+test("Handle multiple windows", async ({ page, context }) => {
+  await page.goto("https://the-internet.herokuapp.com/windows");
 
-    await newPage.close();
-    
-    console.log("Parent page title: ", await page.title());
-    
-   
+  console.log("Parent Page Title:", await page.title());
 
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    page
+      .getByRole("link", {
+        name: "Click Here",
+      })
+      .click(),
+  ]);
+
+  await newPage.waitForLoadState();
+
+  expect(context.pages().length).toBe(2);
+
+  await expect(newPage.locator("h3")).toHaveText("New Window");
+
+  console.log("New Page Title:", await newPage.title());
+
+  await newPage.close();
+
+  expect(context.pages().length).toBe(1);
+
+  console.log("Parent Page Title:", await page.title());
 });
